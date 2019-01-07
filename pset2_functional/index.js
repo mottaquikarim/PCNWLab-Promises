d0ee13a033cce488bc1b38f69bb8aeb96ae7fb72
Robert Abreu <robertabreu@pursuit.org>
2019-01-06 23:17:38 -0500

    solution: problem 4 PSET2, concatFiles

--- pset2_functional/index.js
+++ pset2_functional/index.js
@@ -43,58 +43,53 @@ const copyFile = (fileName) => {
 // const copyFile2 = fileName => readFilePromise(fileName)
 //     .then(data => writeFilePromise(`${fileName}_copy`, data))
 
-copyFile2('index.js').then(_ => console.log('look ma im done'))
+// copyFile2('index.js').then(_ => console.log('look ma im done'))
 
-const concatFiles = (fileNames, newFile) => {
-     
 
-    return new Promise((resolve, reject) => {
-        const newData = [];
-        for (let i = 0; i < fileNames.length; i++) {
-            console.log(`in for loop`)
 
-            const currentFile = fileNames[i];
 
-            readFilePromise(currentFile)
-                .then((data) => {
-                    console.log(data);
-                    newData.push(data);
-                }).catch(() => {
-                    console.log('failed to read', currentFile);
-                });
-        };
 
-        console.log(`past for loop`)
+const arr = ['test', 'test_copy', 'test2', 'test3'];
 
-        writeFilePromise(newFile, newData)
-        .then(() => {
-            console.log(`write for loop promise`)
-            resolve();
-        }).catch(() => {
-            reject();
-        });
+const concatFiles = (arr) => {
 
-    });
+    const newData = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        const currentFile = arr[i]
+
+        const f = readFilePromise(currentFile)
+        newData.push(f);
+    };
+
+    return Promise.all(newData)
 };
 
 
-writeFilePromise('test2', {
-        name: 'Robert',
-        age: 30
-    }).then( () => {
-        return writeFilePromise('test3', {
-            name: 'Robert',
-            age: 30
-        });
-    }).then(() => {
-        return concatFiles(['test', 'test_copy', 'test2', 'test3'], 'concat');
-    }).then(() => {
-        console.log('All files should have been concated')
-        // return readFilePromise('concat');
-    }).catch(()=> {
-        console.log('Files Failed to concat');
-    })
+concatFiles(arr).then( (data)=> {
+    console.log(data.reduce((acc,e) => {
+        acc += JSON.stringify(e);
+        return acc;
+    }, ''))
+});
+
+
+// writeFilePromise('test2', {
+//         name: 'Robert',
+//         age: 30
+//     }).then( () => {
+//         return writeFilePromise('test3', {
+//             name: 'Robert',
+//             age: 30
+//         });
+//     }).then(() => {
+//         return concatFiles(['test', 'test_copy', 'test2', 'test3'], 'concat');
+//     }).then(() => {
+//         console.log('All files should have been concated')
+//         // return readFilePromise('concat');
+//     }).catch(()=> {
+//         console.log('Files Failed to concat');
+//     })
 
 // writeFilePromise('test', {
 //     name: 'Robert',
